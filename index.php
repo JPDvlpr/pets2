dca028685905850d251cccd00cd700f1224eaf5c
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-01-25 17:44:15 -0500

    add updated index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  * Time: 1:30 PM
  */
 
+session_start();
 //error reporting
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
@@ -19,13 +20,11 @@ $f3->set('DEBUG', 3);
 
 $f3->route('GET /',
     function () {
-        echo'default';
+        echo 'default';
     }
 );
 
-
-
-$f3->route('GET /pets2/show/@animal', function ($f3, $params) {
+$f3->route('GET /pets/show/@animal', function ($f3, $params) {
     switch ($params['animal']) {
         case 'cat':
             echo "<img src='../../images/cat.jpg' alt='cat'>";
@@ -36,13 +35,39 @@ $f3->route('GET /pets2/show/@animal', function ($f3, $params) {
             //echo 'dogs';
             break;
         case 'cow':
-            $f3->reroute('/');
+            //$f3->reroute('/');
 //        404 error
         default:
             //$f3->error(404);
             echo 'neither animal';
     }
 });
+
+$f3->route('GET /pets/order',
+    function ($f3, $params) {
+        $template = new Template();
+        echo $template->render('form1.html');
+    }
+);
+
+$f3->route('POST /pets/order2',
+    function ($f3,$params) {
+        $_SESSION['animals'] = $_POST['animals'];
+        $template = new Template();
+        echo $template->render('form2.html');
+    }
+);
+
+$f3->route('POST /pets/results',
+    function ($f3,$params) {
+    //shows every variable within the session
+    //var_dump($_SESSION);
+        $_SESSION['color'] = $_POST['color'];
+        $f3-> set('animals', $_SESSION['animals']);
+        $f3-> set('color', $_SESSION['color']);
+        $template = new Template();
+        echo $template->render('results.html');    }
+);
 
 //run fat free
 $f3->run();
